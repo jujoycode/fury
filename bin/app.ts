f45c64d8d2d7f1e634c9bc871c1542330dbcd496
jujoycode <jh.b.ryu@gmail.com>
2024-04-21 22:32:01 +0900

    edit : add option

--- bin/app.ts
+++ bin/app.ts
@@ -1,46 +1,49 @@
 #!/usr/bin/env node
 
-// package.json
-// import myPackage from '../package.json'
 // commander
-// import { program } from 'commander'
+import { createCommand } from 'commander'
+// figlet
+import { textSync } from 'figlet'
 // generator
 import { Generator } from "./src/generator"
 // interface
-// import { ProgramOption } from './src/interface';
+import { ProgramOption } from './src/interface';
 
 // program setting
-// program
-//   .name('fury')
-//   .description(myPackage.description)
-//   .option('no option', 'Start create project')
-//   .option('-pa', 'Commit all changes', false)
-//   .argument('[commitMessage]', '') //args[0]
-//   .version(myPackage.version)
-//   .parse()
+const program = createCommand();
 
 
-// const options = program.opts<ProgramOption>();
-// const args = program.args;
+program
+  .name('fury')
+  .version('1.0.2')
+  .description('Project generator for Node.js, supports various templates.')
+  .option('no option', 'Start create project')
+  .option('-pa', 'Commit all changes', false)
+  .argument('[commitMessage]', '') //args[0]
+  .parse();
+
+
+const options = program.opts<ProgramOption>();
+const args = program.args;
 
 // program start
 (async () => {
   try {
-    await Generator()
-    // switch (true) {
-    //   case options.Pa: {
-    //     console.log(args)
-    //     break
-    //   }
-    //   default: {
-    //     await Generator()
-    //     break
-    //   }
-    // }
+    console.log(`${textSync(`Fury - 1.0.2`)}\n`)
+    switch (true) {
+      case options.Pa: {
+        console.log(args)
+        break
+      }
+      default: {
+        await Generator()
+        break
+      }
+    }
   }
   // global catch
   catch (error: any) {
-    console.log('\n----------------- ⚠️  Error ⚠️  -----------------\n')
+    console.log('\n---------------- ⚠️  Error ⚠️  ------------------\n')
     console.error(error.message)
     console.log('\n-----------------------------------------------')
   }
